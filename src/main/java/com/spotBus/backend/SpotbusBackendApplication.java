e2943ed8fc3300865ff1e2293a7e223cd6e9dd2d
Vaibhav <vaibhavgupta2980702@gmail.com>
2026-05-15 04:55:03 +0530

    Initial commit

---
+++ src/main/java/com/spotBus/backend/SpotbusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spotBus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotbusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotbusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotBus/spotbus_backend/SpotbusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotBus.spotbus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotbusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
